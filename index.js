521303decb8ceda4ffe49943ba4d1d7eeeae87f9
pulokDAS301 <pupallob@gmail.com>
2024-02-27 16:43:25 +0600

    finally dane

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 
 const allBtn = document.getElementsByClassName("add-btn");
-const buttonApply = document.getElementsByClassName("buttonApply");
-buttonApply.addEventListener.disabled = true;
+
 
 
 
